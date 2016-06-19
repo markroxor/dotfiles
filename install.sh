c27a02699dee34f78affbc4ec6b636f05e90cc59
markroxor <mrmohitrathoremr@gmail.com>
2016-06-19 13:43:26 +0530

    added submodule zsh

--- install.sh
+++ install.sh
@@ -1,10 +1,51 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# bootstrap.sh
+# Symlink all dotfiles in this repo to $HOME.
+#
+# Brandon Amos
+# 2013.01.20
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+set -e
 
-#change default shell
-chsh -s /bin/zsh
-ln -sv “~/.dotfiles/runcom/.bashrc” ~
-ln -sv “~/.dotfiles/runcom/.zshrc” ~
-ln -sv “~/.dotfiles/git/.gitconfig” ~
-ln -sv “~/.dotfiles/git/.gitignore” ~
+link_file() {
+  local ORIG="$1"; local NEW="$2"
+
+  echo "  '$ORIG'"
+  if [[ $BACKUP =~ ^[Yy]$ ]]; then
+    mv "$ORIG" "${ORIG}.prebamos" &> /dev/null \
+    && echo "    ...backed up"
+  else
+    rm -rf "$ORIG"
+    echo "    ...deleted"
+  fi
+  ln -s "$NEW" "$ORIG" && echo "    ...linked"
+}
+
+if [[ $# == 1 ]]; then
+  [[ $1 =~ '-y' ]] && BACKUP='y'
+  [[ $1 =~ '-n' ]] && BACKUP='n'
+else
+  read -p "Backup files? (y/n) " -n 1; echo
+  BACKUP=$REPLY
+fi
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+echo "Symlinking..."
+
+for DOTFILE in $(find . -maxdepth 1 -name '.?*'); do
+  [[ $DOTFILE != "./.git" ]] \
+    && [[ $DOTFILE != "./.gitmodules" ]] \
+    && [[ $DOTFILE != "./.gitignore" ]] \
+    && [[ $DOTFILE != "./.config" ]] \
+    && [[ $DOTFILE != "./screenshots" ]] \
+    && [[ ! $DOTFILE =~ swp$ ]] \
+    && link_file "$HOME/$DOTFILE" "$PWD/$DOTFILE"
+done
+
+mkdir -p $HOME/.config
+cd .config
+for DOTFILE in $(find . -maxdepth 1 ! -path .); do
+  link_file "$HOME/.config/$DOTFILE" "$PWD/$DOTFILE"
+done
