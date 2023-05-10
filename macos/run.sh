120f156cdf674f7fc90991b57c5d8f1b416d2935
Mohit Rathore <mrmohitrathoremr@gmail.com>
2023-05-10 09:48:35 -0700

    update brew install command

--- macos/run.sh
+++ macos/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # wget
 brew install wget
